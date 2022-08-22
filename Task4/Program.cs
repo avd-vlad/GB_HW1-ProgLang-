abbce4a69e45c81e6e5f398c499d2a94ac3451ed
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-22 13:30:10 +0300

    Task4 is solved

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Welcome to the \"Show all even from 1 to Num\" app!"); //Приветсвие
+
+Console.Write("Please enter the number to check:"); // Запрашиваем у пользователя число N
+
+int num = Int32.Parse(Console.ReadLine());
+
+int num_count = 0;                                  // Переменная для форматирования вывода в консоль
+
+for (int i=1; i<=num; i++)                          // Запускаем цикл от 1 до N
+    if(i%2 == 0)                                    // Проверяем на четность
+    {
+        if(num_count++%10 == 0)                               
+            Console.WriteLine();                    // Через каждые 10 чисел переходим на новую строку
+        Console.Write(i.ToString("000  "));           // Выводим число соответствующее условию задачи в консоль
+    }
+    
+

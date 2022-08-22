e59e6df731ad69b035d116f4438df10f51e22ace
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-22 13:06:31 +0300

    Task3 is solved

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Welcome to the \"Check Odd or Even\" app!"); //Приветсвие
+
+Console.Write("Please enter the number to check:"); // Запрашиваем у пользователя число для проверки
+int a = Int32.Parse(Console.ReadLine());
+
+if ( a % 2 ==0)
+    Console.Write($"The number \"{a}\" you entered is Even"); // Выводим результат проверки на четность
+else
+    Console.Write($"The number \"{a}\" you entered is Odd"); // Выводим результат проверки на четность
+
